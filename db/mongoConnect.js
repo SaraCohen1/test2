eeddd89fa3b107f0324ade61578af0f0f04d22f3
SaraCohen1 <sara149635@gmail.com>
2023-11-29 11:50:29 +0200

    first commit

---
+++ db/mongoConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const {config} = require("../config/secret")
+
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.set('strictQuery' , false);
+
+  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.h6yht2c.mongodb.net/sara25`);
+  console.log("mongo connect black 22 a")
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
+
+
+
